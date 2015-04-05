0aa1b65fdf3fbaead8bb1e66925e3a95b97c19b3
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2015-04-05 02:24:40 +0200

    Make verbose really verbose.

--- ltq-xdsl-fw-info.sh
+++ ltq-xdsl-fw-info.sh
@@ -38,19 +38,24 @@ EOF
 	printf "%s usage:\n%s\n" "${PROG_NAME}" "${HELP_TEXT}"
 }
 
-function print_firmware_features() {
+function parse_firmware_features() {
 	local FW_VERSION="${1}"
 	local FW_DETAILS=(${FW_VERSION//./ })
-	local APPLICATION_TYPE_STR=""
-	local RELEASE_STATUS_STR=""
-	local PLATFORM_STR=""
 
-	local PLATFORM="${FW_DETAILS[0]//[^0-9]/}"
-	local FEATURE_SET="${FW_DETAILS[1]}"
-	local MAJOR_VERSION="${FW_DETAILS[2]}"
-	local MINOR_VERSION="${FW_DETAILS[3]}"
-	local RELEASE_STATUS="${FW_DETAILS[4]}"
-	local APPLICATION_TYPE="${FW_DETAILS[5]}"
+	unexport_firmware_features
+
+	export APPLICATION_TYPE_STR=""
+	export RELEASE_STATUS_STR=""
+	export PLATFORM_STR=""
+
+	export PLATFORM="${FW_DETAILS[0]//[^0-9]/}"
+	export FEATURE_SET="${FW_DETAILS[1]}"
+	export MAJOR_VERSION="${FW_DETAILS[2]}"
+	export MINOR_VERSION="${FW_DETAILS[3]}"
+	export RELEASE_STATUS="${FW_DETAILS[4]}"
+	export APPLICATION_TYPE="${FW_DETAILS[5]}"
+
+	export VERSION_STR="${FEATURE_SET}.${MAJOR_VERSION}.${MINOR_VERSION}"
 
 	case ${APPLICATION_TYPE} in
 		0)
@@ -126,17 +131,42 @@ function print_firmware_features() {
 			RELEASE_STATUS_STR="UNKONWN release status (${RELEASE_STATUS})"
 			;;
 	esac
+}
 
-	printf "%s for %s, version: %s.%s.%s" \
-		"${APPLICATION_TYPE_STR}" "${PLATFORM_STR}" \
-		"${FEATURE_SET}" "${MAJOR_VERSION}" "${MINOR_VERSION}"
+function unexport_firmware_features() {
+	unset APPLICATION_TYPE_STR
+	unset RELEASE_STATUS_STR
+	unset PLATFORM_STR
 
-	if [ "${VERBOSE}" -eq "1" ]
-	then
-		printf " (VERBOSE: raw version: %s, %s version)" "${FW_VERSION}"  "${RELEASE_STATUS_STR}"
-	fi
+	unset PLATFORM
+	unset FEATURE_SET
+	unset MAJOR_VERSION
+	unset MINOR_VERSION
+	unset RELEASE_STATUS
+	unset APPLICATION_TYPE
 
-	printf "\n"
+	unset VERSION_STR
+}
+
+function print_firmware_features() {
+	printf "%s for %s, version: %s" \
+		"${APPLICATION_TYPE_STR}" \
+		"${PLATFORM_STR}" \
+		"${VERSION_STR}"
+}
+
+function print_firmware_features_verbose() {
+	printf "	PLATFORM: %s\n" "${PLATFORM}"
+	printf "	PLATFORM_STR: %s\n" "${PLATFORM_STR}"
+	printf "	APPLICATION_TYPE: %s\n" "${APPLICATION_TYPE}"
+	printf "	APPLICATION_TYPE_STR: %s\n" "${APPLICATION_TYPE_STR}"
+	printf "	VERSION_STR: %s\n" "${VERSION_STR}"
+	printf "	RELEASE_STATUS: %s\n" "${RELEASE_STATUS}"
+	printf "	RELEASE_STATUS_STR: %s\n" "${RELEASE_STATUS_STR}"
+}
+
+function get_sha1sum() {
+	sha1sum "${1}" | cut -d' ' -f1
 }
 
 while getopts "p:vh" OPT; do
@@ -170,16 +200,58 @@ do
 
 			if [ "${FILE_HEADER//[^ELF]/}" = "ELF" ]
 			then
-				echo "${FILENAME}: Driver / tool version ${VERSIONS[0]}"
+				printf "%s: Driver / tool version %s" \
+					"${FILENAME}" \
+					"${VERSIONS[0]}"
 			else
-				echo "${FILENAME}: $(print_firmware_features "${VERSIONS[0]}")"
+				parse_firmware_features "${VERSIONS[0]}"
+
+				if [ "${VERBOSE}" -eq "1" ]
+				then
+					printf "filename: %s\nversion: %s\nsha1sum: %s\n" \
+						"${FILENAME}" \
+						"${VERSIONS[0]}" \
+						"$(get_sha1sum "${FILE}")"
+					print_firmware_features_verbose
+				else
+					printf "%s: " "${FILENAME}"
+					print_firmware_features
+				fi
 			fi
 			;;
 		2)
-			echo "${FILENAME}: $(print_firmware_features "${VERSIONS[0]}") | $(print_firmware_features "${VERSIONS[1]}")"
+			parse_firmware_features "${VERSIONS[0]}"
+
+			if [ "${VERBOSE}" -eq "1" ]
+			then
+				printf "filename: %s\nversion: %s\nsha1sum: %s\n" \
+					"${FILENAME}" \
+					"${VERSIONS[0]}-${VERSIONS[1]}" \
+					"$(get_sha1sum "${FILE}")"
+				echo "Firmware1:"
+				print_firmware_features_verbose
+			else
+				printf "%s: " "${FILENAME}"
+				print_firmware_features
+			fi
+
+			parse_firmware_features "${VERSIONS[1]}"
+
+			if [ "${VERBOSE}" -eq "1" ]
+			then
+				echo "Firmware2:"
+				print_firmware_features_verbose
+			else
+				printf " | "
+				print_firmware_features
+			fi
 			;;
 		*)
-			echo "${FILENAME}: NO firmware versions found - is this a valid lantiq DSL firmware file?"
+			printf "NO firmware versions found - is this a valid lantiq DSL firmware file?"
 			;;
 	esac
+
+	unexport_firmware_features
+
+	printf "\n"
 done
